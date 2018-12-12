6cebbdae1a9e5521de98d38819797b0dddfd1f80
Karl Burnett <burnett.karl@gmail.com>
2018-12-11 20:35:53 -0800

    Deleted test script from conceptual

--- conceptual.py
+++ conceptual.py
@@ -164,24 +164,3 @@ class EmptyWeight:
 class TakeoffWeight:
     def __init__(self):
         return
-
-
-taxi = Phase(3, 15, 30, 0, 3)
-takeoff = Phase(13.4, 15, 10, 0, 13.4)
-climb = Phase(22.4, 10, 48, 2.54, 9)
-endurance = Phase(22.4, 20, 1800, 0, 0)
-descent = Phase(13.4, 15, 48, -2.54, -9)
-pattern = Phase(13.4, 10, 60, 0, 0)
-land = Phase(0, 5, 15, -1, -13.4)
-droan_mission = Mission(12.5)
-# I feel like compile_unique_phases could be automated fairly easily
-droan_mission.compile_unique_phases([taxi, takeoff, climb, endurance, descent, pattern, land])
-droan_mission.add_all_phases([taxi, takeoff, climb, endurance, descent, pattern, land, taxi])
-droan_power_per_phase = PhasePower(droan_mission)
-print([math.ceil(phase.maximum_power) for phase in droan_mission.unique_phases])
-print("Maximum mission power is " + str(math.ceil(droan_mission.maximum_power)) + " watts.")
-droan_motor = Motor(11.1, 0.8, 110)
-droan_battery = Battery(11.1, 25, 2.2, 140)  # Gens ace 25C 2200mah 11.1V 3S Lipo Battery
-print("Each battery has a mass of " + str(droan_battery.battery_cell_mass) + " kilograms")
-droan_battery_pack_mass = BatteryPackMass(droan_motor, droan_mission, droan_battery).battery_pack_mass
-print("Total battery pack mass is " + str(droan_battery_pack_mass) + " kilograms")
