d7ad1488a823b43fa1e66a30348ebfa387a13798
Karl Burnett <burnett.karl@gmail.com>
2018-12-11 19:51:10 -0800

    Corrected equations to have similar units, i.e. Amp hours to Amp seconds

--- conceptual.py
+++ conceptual.py
@@ -30,6 +30,8 @@ class Mission:
         self.all_phases = []
         self.unique_phases = []
         self.takeoff_weight_guess = takeoff_weight_guess
+        # Lowest voltage ratio at which you still want to execute max power, i.e. 0.8 is 80% of full charge voltage
+        self.lowest_voltage_maximum_power_ratio = 0.75
         self.maximum_power = None
 
     def add_all_phases(self, phases):
@@ -70,8 +72,8 @@ class Battery:
         """
         self.nominal_cell_voltage = nominal_cell_voltage
         self.c_max = c_max
-        self.cell_capacity = cell_capacity
-        self.specific_energy_density = specific_energy_density
+        self.cell_capacity = cell_capacity * 3600  # Convert ampere hours to ampere seconds
+        self.specific_energy_density = specific_energy_density * 3600  # Convert W-h/kg to W-s/kg
         self.battery_cell_mass = self.cell_capacity * self.nominal_cell_voltage / self.specific_energy_density
 
 
@@ -114,41 +116,41 @@ class PhasePower:
         else:
             return max(final_speed, initial_speed)
 
-# Still need to modify BatteryPackMass based on our conversation on 11 Dec 2018
-
 
 class BatteryPackMass:
     """ The aircraft battery is sized to execute the provided mission with appropriate power and capacity. """
 
-    def __init__(self, maximum_power, motor, mission, battery):
+    def __init__(self, motor, mission, battery):
         self.number_in_series = self.size_number_in_series(motor.input_voltage, battery.nominal_cell_voltage)
-        self.number_in_parallel = self.size_number_in_parallel(maximum_power, battery,
-                                                               motor, mission)
+        self.number_in_parallel = self.size_number_in_parallel(battery, motor.whole_chain_efficiency,
+                                                               mission.all_phases, mission.maximum_power,
+                                                               mission.lowest_voltage_maximum_power_ratio)
         self.number_of_cells = self.number_in_series * self.number_in_parallel
         self.battery_pack_mass = self.number_of_cells * battery.battery_cell_mass
 
     def size_number_in_series(self, input_voltage, nominal_cell_voltage):
+        print(str(math.ceil(input_voltage / nominal_cell_voltage)) + " batteries in series.")
         return math.ceil(input_voltage / nominal_cell_voltage)
 
-    def size_number_in_parallel(self, phase_power, battery, motor, mission):
-        power = self.size_parallel_for_power(phase_power, battery, motor)
-        endurance = self.size_parallel_for_endurance(phase_power, battery, motor, mission)
-        return max(power, endurance)
+    def size_number_in_parallel(self, battery, efficiency, all_phases, max_power, low_voltage_ratio):
+        parallel_power = self.size_parallel_for_power(max_power, battery.nominal_cell_voltage, efficiency,
+                                                      battery.c_max, battery.cell_capacity, low_voltage_ratio)
+        parallel_endurance = self.size_parallel_for_endurance(battery.nominal_cell_voltage, battery.cell_capacity,
+                                                              efficiency, all_phases)
+        print(str(math.ceil(max(parallel_power, parallel_endurance))) + " batteries in parallel.")
+        return math.ceil(max(parallel_power, parallel_endurance))
 
-    def size_parallel_for_endurance(self, phase_power, battery, motor, mission):
-        phase_energy = [phase_power.power_mission_phase[phase] * mission.phases[phase][2]
-                        for phase in range(mission.unique_phases)]
-        cell_energy_capacity = battery.nominal_cell_voltage * battery.cell_capacity * motor.whole_chain_efficiency
-        phase_parallel = [phase_energy[phase] / cell_energy_capacity for phase in range(mission.unique_phases)]
-        return sum(phase_parallel)
+    def size_parallel_for_endurance(self, cell_voltage, cell_capacity, efficiency, phases):
+        phase_energy = [specific_phase.maximum_power * specific_phase.time for specific_phase in phases]
+        cell_energy_capacity = cell_voltage * cell_capacity * efficiency
+        print(str(math.ceil(sum(phase_energy) / cell_energy_capacity)) + " batteries in endurance parallel")
+        return math.ceil(sum(phase_energy) / cell_energy_capacity)
 
-    def size_parallel_for_power(self, phase_power, battery, motor):
-        max_power = phase_power.maximum_power
-        c_max = battery.c_max
-        cell_capacity = battery.cell_capacity
-        whole_chain_efficiency = motor.whole_chain_efficiency
-        battery_pack_voltage = self.number_in_series * battery.nominal_cell_voltage
-        return max_power / (c_max * cell_capacity * whole_chain_efficiency * battery_pack_voltage)
+    def size_parallel_for_power(self, max_power, cell_voltage, efficiency, c_max, cell_capacity, low_voltage_ratio):
+        battery_pack_voltage = self.number_in_series * cell_voltage * low_voltage_ratio
+        print(str(math.ceil(max_power / (c_max * cell_capacity * efficiency * battery_pack_voltage)))
+              + " batteries in power parallel")
+        return math.ceil(max_power / (c_max * cell_capacity * efficiency * battery_pack_voltage))
 
 
 class EmptyWeight:
@@ -173,9 +175,9 @@ droan_mission = Mission(12.5)
 droan_mission.compile_unique_phases([taxi, takeoff, climb, endurance, descent, pattern, land])
 droan_mission.add_all_phases([taxi, takeoff, climb, endurance, descent, pattern, land, taxi])
 droan_power_per_phase = PhasePower(droan_mission)
-print(droan_mission.maximum_power)
+print([phase.maximum_power for phase in droan_mission.unique_phases])
 droan_motor = Motor(11.1, 0.8, 110)
-droan_battery = Battery(3.7, 25, 0.5, 200)
-droan_battery_pack_mass = BatteryPackMass(droan_power_per_phase, droan_motor, droan_mission,
-                                          droan_battery).battery_pack_mass
-print(droan_battery_pack_mass)
+droan_battery = Battery(11.1, 25, 2.2, 140)  # Gens ace 25C 2200mah 11.1V 3S Lipo Battery
+print("Each battery has a mass of " + str(droan_battery.battery_cell_mass) + " kilograms")
+droan_battery_pack_mass = BatteryPackMass(droan_motor, droan_mission, droan_battery).battery_pack_mass
+print("Total battery pack mass is " + str(droan_battery_pack_mass) + " kilograms")
